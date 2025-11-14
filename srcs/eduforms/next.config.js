8264d6ea1fef8b0fb24f97c04cf62ec10187166b
Tomas Lapes <tom@x-com.cz>
2025-11-14 04:18:48 +0100

    Fix docker file

--- srcs/eduforms/next.config.js
+++ srcs/eduforms/next.config.js
@@ -6,6 +6,7 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
+  output: 'standalone',
   typescript: {
     // TODO fix type errors
     ignoreBuildErrors: true,
