81b5fc8b196b410f3ea1140d7ad1742e3b84eb70
Lilia <nikolovalilianenkova@gmail.com>
2025-11-14 03:44:52 +0100

    presentation halfway done

---
+++ hackathon-presentation/js/slides-story.js
@@ -0,0 +1,30 @@
+export const storySlides = [
+  {
+    title: "The Beginning",
+    render: () => `
+      <p>
+        We designed a solution that goes beyond managing current data. It creates clean, controlled, and reliable data flows that reduce errors, improve performance, and deliver faster, more accurate insights.
+Our system is built to grow with the schools—ensuring consistent data quality and accelerating both daily functionality and long-term, region-wide improvement.<br/><br/>
+      </p>
+    `
+  },
+  {
+    title: "Story · The Challenge",
+    render: () => `
+      <p>
+        Schools are struggling to keep up with modern reality. Technology is evolving fast, but the systems that support students and teachers are not. 
+        Most schools still rely on outdated tools and data processes that make it hard to track progress, personalize learning, or make informed decisions. 
+        Massive amounts of information exist, but collecting, maintaining, and using it effectively takes years — creating delays, errors, and missed opportunities.
+      </p>
+    `
+  },
+  {
+    title: "Story · The Breakthrough",
+    render: () => `
+      <p>
+        What was the “aha” moment?<br/><br/>
+        Describe how your team connected the dots and came up with your solution.
+      </p>
+    `
+  }
+];
